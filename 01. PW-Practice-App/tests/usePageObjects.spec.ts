73e269ac29dacea9a6f666607ff27983fca50f76
Ivan Davidov <ivandavidov13@yahoo.com>
2024-06-12 15:21:51 +0300

    Test Data Generation

--- 01. PW-Practice-App/tests/usePageObjects.spec.ts
+++ 01. PW-Practice-App/tests/usePageObjects.spec.ts
@@ -1,5 +1,6 @@
 import { expect, test } from "@playwright/test";
 import { PageManager } from "../page-objects/pageManager";
+import { faker } from "@faker-js/faker";
 
 test.beforeEach(async ({ page }) => {
   await page.goto("http://localhost:4200/");
@@ -16,6 +17,10 @@ test("navigate to form page", async ({ page }) => {
 
 test("parametrized methods", async ({ page }) => {
   const pm = new PageManager(page);
+  const randomFullName = faker.person.fullName();
+  const randomEmail = `${randomFullName.replace(" ", "")}${faker.number.int(
+    1000
+  )}@test.com`;
 
   await pm.navigateTo().formLayoutsPage();
   await pm
@@ -29,8 +34,8 @@ test("parametrized methods", async ({ page }) => {
   await pm
     .onFormLayoutsPage()
     .submitInlineFormWithNameEmailAndCheckbox(
-      "John Smith",
-      "John@test.com",
+      randomFullName,
+      randomEmail,
       true
     );
   await pm.navigateTo().datepickerPage();
